c6b95b25e48921c171dc7688b67fc17412401c8f
Mohamed <momoserhir@gmail.com>
2019-12-20 23:46:43 +0100

    up

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 """
 Created on Wed Dec 11 14:10:47 2019
 
-@author: SERHIR Mohamed
+@author: SERHIR Mohamed & ZARGA Ines
 """
+
 import math
+import numpy as np
+from sympy import symbols, Eq, solve
 
 # main python program
-response = ['Welcome to smart calculator', 'Hello user !', 'Thanks for enjoy with me',
-            'Sorry ,this is beyond my ability']
+response = ['Welcome to smart calculator', 'Hello ', 'Thanks for enjoy with me',
+            'Sorry ,this is beyond my ability', 
+            'To play with this calculator, write what you want to do, for example add or subtract', 
+            'If you want to know every command, tap help']
 
 
 # fetching tokens from the text command
@@ -18,11 +23,14 @@ def extract_from_text(text):
             t = math.pi
         elif t.upper() == "E":
             t = math.e
+        elif t.upper() == "LAST":
+            t = lastQuery
         try:
             l.append(float(t))
         except ValueError:
             pass
     return l
+
 
 
 # calculating LCM
@@ -100,6 +108,7 @@ def a_tan(a):
 
 # Puissance
 def power(a, b):
+    #print(a,b, "pow")
     return math.pow(a, b)
 
 
@@ -163,16 +172,165 @@ def find_delta(a, b, c):
     return b * b - 4 * a * c
 
 
+# To find gcd
+def gdc(a, b):
+    return math.gcd(int(a),int(b))
+
+
+# To convert to degree
+def degree(a):
+    return math.degrees(a)
+
+
+# To convert to radian
+def radian(a):
+    return math.radians(a)
+
+
+# To print the next var (usefull for pi for example)
+def print_var(a):
+    return a
+
+# TVA HT
+def tva_ttc_to_ht(a):
+    tva_percent = input("Percent of the tva ?: ")
+    return a / (1 + float(tva_percent) / 100)
+
+
+# TVA TTC
+def tva_ht_to_ttc(a):
+    tva_percent = input("Percent of the tva ?: ")
+    return a * (float(tva_percent) / 100) + a
+
+
 # Equation du second degre
 def sde(a, b, c):
-    delta = find_delta(a, b, c)
-    if delta > 0:
-        result = "x1 = " + str((-b - math.sqrt(delta)) / (2 * a)) + " and x2 = " + str((-b + sqrt(delta)) / (2 * a))
-    elif delta < 0:
-        result = "x1 = " + str((-b) / (2 * a)) + " -i" + str(math.sqrt(-delta) / (2 * a)) + " and x2 = " \
-                 + str((-b) / (2 * a)) + " +i" + str(math.sqrt(-delta) / (2 * a))
+    if a != 0 and b != 0 and c != 0:
+        print("Your equation: ", a, "x²+", b,"x+",c)
+        rep = input("yes or no ? (y / n)\n")
+        if rep == "n" or rep == "no":
+            return "Nothing"
+        #calcul of delta 
+        delta = find_delta(a, b, c)
+
+        # coeff = [a,b,c]
+        # print(np.roots(coeff)[0]) possible avec numpy mais affichage meilleur ci dessous:
+        if delta > 0:
+            result = "x1 = " + str((-b - math.sqrt(delta)) / (2 * a)) + " and x2 = " + str((-b + sqrt(delta)) / (2 * a))
+        elif delta < 0:
+            result = "x1 = " + str((-b) / (2 * a)) + " -" + str(round(math.sqrt(-delta) / (2 * a),2)) + "i and x2 = " \
+                    + str((-b) / (2 * a)) + " +" + str(round(math.sqrt(-delta) / (2 * a),2)) + "i"
+        else:
+            result = [-b / (2 * a)]
     else:
-        result = [-b / (2 * a)]
+        #Input the 3 variable of the equation
+        print("Please enter your equation ax² + bx + c with a > 0")
+        a = input("a= ")
+        b = input("b= ")
+        c = input("c= ")
+        #verify if the equation is good
+        print("Your equation: ", a, "x²+", b,"x+",c)
+        rep = input("yes or no ? (y / n)\n")
+
+        if rep == "n" or rep == "no":
+            return "Nothing"
+        #verify if the 3 variables are numbers
+        try:
+            a = float(a)
+            b = float(b)
+            c = float(c)
+        except: #if not, re enter the 3 variables
+            print("Please do it again (your vars are not correct...)")
+            return ""
+
+        delta = find_delta(a, b, c)
+        if delta > 0:
+            x1 = - 1 * b - math.sqrt(delta) / (2 * a)
+            x2 = (-b + sqrt(delta)) / (2 * a)
+            result = "x1 = " + str(x1) + " and x2 = " + str(x2)
+        elif delta < 0:
+            result = "x1 = " + str((-b) / (2 * a)) + " -" + str(round(math.sqrt(-delta) / (2 * a),2)) + "i and x2 = " \
+                    + str((-b) / (2 * a)) + " +" + str(round(math.sqrt(-delta) / (2 * a),2)) + "i"
+        else:
+            result = [-b / (2 * a)]
+
+        '''
+        x = array([1, 3, 4, 6])
+        y = array([2, 3, 5, 1])
+        plot(x, y)
+
+        show() 
+        '''
+        
+    return result
+
+# Equation 2 unknown
+def equation(a,b,c,d,e,f):
+    #if we enter "equation a b c d e" :
+    if a != 0 and b != 0 and c != 0 and d != 0 and e != 0 and f != 0 :
+        #display the 2 equations
+        print("Your equations: ")
+        print(a,"x +", b, "y = ", c)
+        print(d,"x +", e, "y = ", f)
+        rep = input("yes or no ? (y / n)\n")
+        if rep == "n" or rep == "no":
+            return "Nothing"
+        #We use the import symbols, Eq, solve for solve the two equation
+        x, y = symbols('x y')
+
+        #define the two equations :
+        eq1 = Eq(a*x +b*y + c)
+        eq2 = Eq(d*x + e*y + f)
+        #solve te equation eq1 eq2 with the variable x and y 
+        sol_dict = solve((eq1,eq2), (x, y))
+
+        if a * d - b*c == 0:
+            print("The determinant is null, so the equations can't be resolve \n a * d - b*c = 0")
+
+        #display the result which is in the dictionary sol_dict 
+        result = f'x = {sol_dict[x]}' + "and" + f'y = {sol_dict[y]}'
+    else:
+        print("Please enter the 2 equations :")
+        print("ax + by = c")
+        print("dx + ey = f")
+        #Input the 6 variable of the equation
+        a = input("a= ")
+        b = input("b= ")
+        c = input("c= ")
+        d = input("d= ")
+        e = input("e= ")
+        f = input("f= ")
+
+        print("Your equations: ")
+        print(a,"x +", b, "y = ", c)
+        print(d,"x +", e, "y = ", f)
+
+        rep = input("yes or no ? (y / n)\n")
+        if rep == "n" or rep == "no":
+            return "Nothing"
+        #verify if the 6 variables are numbers
+        try:
+            a = float(a)
+            b = float(b)
+            c = float(c)
+            d = float(d)
+            e = float(e)
+            f = float(f)
+        except:
+            print("Please do it again (your vars are not correct...)")
+            return ""
+        x, y = symbols('x y')
+
+        eq1 = Eq(a*x +b*y + c)
+        eq2 = Eq(d*x + e*y + f)
+        #verify if the determinant is null or not 
+        if a * d - b*c == 0:
+            print("The determinant is null, so the equations can't be resolve \n a * d - b*c = 0")
+
+        sol_dict = solve((eq1,eq2), (x, y))
+
+        result = f'x = {round(sol_dict[x],2)}' + " and " + f'y = {sol_dict[y]}'
+        
     return result
 
 
@@ -184,83 +342,141 @@ def end():
     exit()
 
 
-def my_name():
-    print(response[1])
-
+def my_name(name):
+    print(name)
 
 def sorry():
     print(response[3])
 
+def version():
+    print ("Version 1.0 by Serhir & Zarga !")
+
+def help_():
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("You can type what you need, this calculator can do the following queries: \
+        \n- With 1 value: \n  Cos,  Sin, Tan, acos, asin, atan, Puissance, Log, Ln, Exp, Radian, Degree, ttc, ht, factorial, percent \
+        \n     Example: Sin 5 \
+        \n- With 2 values: \n  Add, Sub, LCM, HCF, Mutiplication, Division, Modulo, Racine_Nieme, gcd ( + and - working too) \
+        \n     Example: Please add 7 and 7 \
+        \n- With 3 values: \n  SDE (second-degree equation) \
+        \n     Example: Sde 6 8 -5 \
+        \n- With 6 values: \n  Equation (2 equation solution) \
+        \n     For: ax + by = c and dx + ey = f: \
+        \n     Example: Equation 6 8 -5 2 -7 6 \
+        \nYou can use only sde keyword or equation and follow the instructions \
+        \n If you need to do more operation you can use the functionnality last, for example you can add 1 and 1 and do last + 10, \
+        \n  last is the last operation in memory and the result will be 12\
+        \nYou can find delta by typing delta a b c, with abc your values\
+        \nYou can print your last operation by typing print\
+        \nYou can also quit by typing the keyword quit or exit, and you can know your name by typing name")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 
 # Operations - performed on the basis of text tokens
-operations = {'ADD': add, 'PLUS': add, 'SUM': add, 'ADDITION': add, 'AJOUTER': add, "AJOUTE": add,
-              'SUB': sub, 'SUBTRACT': sub, 'MINUS': sub, 'DIFFERENCE': sub,
-              'LCM': lcm, 'PPCM': lcm,
-              'HCF': hcf, 'PGCD': hcf,
-              'PRODUCT': mul, 'MULTIPLY': mul, 'MULTIPLICATION': mul,
-              'DIVISION': div, 'DIVIDE': div, 'DIVISE': div,
-              'MOD': mod, 'REMANDER': mod, 'MODULAS': mod,
-              'RACINE_NIEME': n_sqrt}
-
-operationsSimpleVar = {'COS': cos, 'COSINUS': cos,
+operationsSimpleVar = {'COS': cos, 'COSINUS': cos, 'COSINE': cos,
                        'SIN': sin, 'SINUS': sin,
-                       'TAN': tan, 'TANGENTE': tan,
-                       'ACOS': a_cos, 'ARCOSINUS': a_cos, 'ARC COSINUS': a_cos, 'COSINUS INVERSE': a_cos,
-                       'COS INVERSE': a_cos,
-                       'ASIN': a_sin, 'ARCSINUS': a_sin, 'ARC SINUS': a_sin, 'SINUS INVERSE': a_sin,
-                       'SIN INVERSE': a_sin,
-                       'ATAN': a_tan, 'ARCTAN': a_tan, 'ARC TANGENTE': a_tan, 'TAN INVERSE': a_tan,
-                       'TANGENTE INVERSE': a_tan,
-                       'PUISSANCE': power, 'POWER': power, 'PUISS': power, 'POW': power,
-                       'PUISSANCE10': power_ten, 'PUISS10': power_ten, 'POW10': power_ten, 'PUISSANCE 10': power_ten,
-                       'LOG': log_ten, 'LOG10': log_ten, 'LOGARITHME10': log_ten, 'LOG DE 10': log_ten,
-                       'LN': log_two, 'LOG2': log_two, 'LOGARITHME2': log_two, 'LOG DE 2': log_two,
-                       'EXP': exp, 'EXPONENTIELLE': exp, 'EXPONENTIEL': exp,
-                       'PUISSANCE 3': x_pow_three, 'PUISSANCE3': x_pow_three, 'PUISS3': x_pow_three,
-                       'POW3': x_pow_three,
-                       'RACINECARREE': sqrt, 'RACINE CARREE': sqrt, 'SQRT': sqrt,
-                       'POURCENTAGE': percent, 'PERCENT': percent}
+                       'TAN': tan, 'TANGENTE': tan, 'TANGENT': tan,
+                       'ACOS': a_cos, 'ARCOSINUS': a_cos, 'ARC_COSINUS': a_cos, 'ARC_COSINE': a_cos,'COS_INVERSE': a_cos, 'COSINUS_INVERSE': a_cos, 'COSINE_INVERSE': a_cos,
+                       'INVERSE_COS': a_cos, 'INVERSE_COSINUS': a_cos, 
+                       'ASIN': a_sin, 'ARCSINUS': a_sin, 'ARC_SINUS': a_sin, 'SINUS_INVERSE': a_sin, 'SIN_INVERSE': a_sin, 'INVERSE_SIN': a_sin, 'INVERSE_SINUS': a_sin,
+                       'ATAN': a_tan, 'ARCTAN': a_tan, 'ARC_TANGENTE': a_tan, 'TAN_INVERSE': a_tan, 'TANGENTE_INVERSE': a_tan,'TANGENT_INVERSE': a_tan, 'INVERSE_TANGENT': a_tan,
+                       'PUISSANCE10': power_ten, 'PUISS10': power_ten, 'POW10': power_ten, 'PUISSANCE_10': power_ten, '^10': power_ten, 'POWER10': power_ten,
+                       'LOG': log_ten, 'LOG10': log_ten, 'LOGARITHME10': log_ten, 'LOG DE 10': log_ten, 'LOGARITHM10': log_ten,
+                       'LN': log_two, 'LOG2': log_two, 'LOGARITHME2': log_two, 'LOG DE 2': log_two, 'LOGARITHM2': log_two,
+                       'EXP': exp, 'EXPONENTIELLE': exp, 'EXPONENTIEL': exp, 'EXPONENTIAL': exp,
+                       'PUISSANCE_3': x_pow_three, 'PUISSANCE3': x_pow_three, 'PUISS3': x_pow_three, '^3': x_pow_three, 'POW3': x_pow_three,
+                       'RACINECARREE': sqrt, 'RACINE_CARREE': sqrt, 'SQRT': sqrt, 'RACINECARRE': sqrt, 'SQUARE_ROOT': sqrt,
+                       'POURCENTAGE': percent, 'PERCENT': percent,
+                       'FACTORIEL': fact, 'FACT': fact, 'FACTORIELLE': fact, "FACTO": fact, 'FACTORIAL': fact,
+                       'PRINT': print_var, 'AFFICHE': print_var,
+                       'RADIAN': radian, 'RADIANS': radian,
+                       'DEGREE': degree, 'DEGREES': degree,
+                       'TTC': tva_ht_to_ttc,
+                       'HT': tva_ttc_to_ht}
 
-operationsThreeVar = {'SDE': sde}
+operationsDoubleVar = {'ADD': add, 'PLUS': add, 'SUM': add, 'ADDITION': add, 'AJOUTER': add, "AJOUTE": add, "+": add, "AND": add,
+              'SUB': sub, 'SUBTRACT': sub, 'MINUS': sub, 'DIFFERENCE': sub, '-': sub, 'ENLEVE': sub, 'ENLEVER': sub,
+              'LCM': lcm, 'PPCM': lcm,
+              'HCF': hcf, 
+              'PRODUCT': mul, 'MULTIPLY': mul, 'MULTIPLICATION': mul, '*': mul,
+              'PUISSANCE': power, 'POWER': power, 'PUISS': power, 'POW': power, '^': power,
+              'DIVISION': div, 'DIVIDE': div, 'DIVISE': div, '/': mul,
+              'MOD': mod, 'REMANDER': mod, 'MODULAS': mod, 'MODULO': mod, '%': mod,
+              'RACINE_NIEME': n_sqrt,
+              'GCD': gdc, 'PGCD': gdc
+              }
 
-# commands
-commands = {'NAME': my_name, 'EXIT': end, 'END': end, 'CLOSE': end}
+operationsThreeVar = {'SDE': sde, 'DELTA': find_delta}
+operationsSixVar = {'EQUATION': equation}
 
-print('----' + response[0] + '-----')
-print('----' + response[1] + '-----')
+
+# Commands
+commands = {'NAME': my_name, 'EXIT': end, 'END': end, 'CLOSE': end, 'QUIT': end, 'HELP': help_, 'VERSION': version}
+
+name = input("What is your name? ")
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('---- \t' + response[0] + '\t\t\t\t\t\t\t\t -----')
+print('---- \t' + response[1]+ name + '\t\t\t\t\t\t\t\t\t\t -----') 
+print('---- \t' + response[4] + '\t -----')
+print('---- \t' + response[5] + '\t\t\t\t\t\t -----')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+lastQuery = 0
 while True:
     print()
-    text = input('enter your queries: ')
+    text = input('Enter your queries: ')
     for word in text.split(' '):
-        if word.upper() in operations.keys():
-            try:
-                l = extract_from_text(text)
-                r = operations[word.upper()](l[0], l[1])
-                print(r)
-            except:
-                print('something went wrong going plz enter again !!')
-            finally:
-                break
-        elif word.upper() in operationsSimpleVar.keys():
+        if word.upper() in operationsSimpleVar.keys():
             try:
                 l = extract_from_text(text)
                 r = operationsSimpleVar[word.upper()](l[0])
+                lastQuery = r
                 print(r)
             except:
-                print('something went wrong going plz enter again !!')
+                print('Something went wrong going please enter again !')
+            finally:
+                break
+        elif word.upper() in operationsDoubleVar.keys():
+            try:
+                l = extract_from_text(text)
+                r = operationsDoubleVar[word.upper()](l[0], l[1])
+                lastQuery = r
+                print(r)
+            except:
+                print('Something went wrong going please enter again !')
             finally:
                 break
         elif word.upper() in operationsThreeVar.keys():
             try:
                 l = extract_from_text(text)
-                r = operationsThreeVar[word.upper()](l[0], l[1], l[2])
+                if len(l) == 3:
+                    r = operationsThreeVar[word.upper()](l[0], l[1], l[2])
+                else:
+                    r = operationsThreeVar[word.upper()](0,0,0)
+                lastQuery = r
                 print(r)
             except:
-                print('something went wrong going plz enter again !!')
+                print('Something went wrong going please enter again !')
+            finally:
+                break
+        elif word.upper() in operationsSixVar.keys():
+            try:
+                l = extract_from_text(text)
+                if len(l) == 6:
+                    r = operationsSixVar[word.upper()](l[0], l[1], l[2], l[3], l[4], l[5])
+                else:
+                    r = operationsSixVar[word.upper()](0,0,0,0,0,0)
+                lastQuery = r
+                print(r)
+            except:
+                print('Something went wrong going please enter again !')
             finally:
                 break
         elif word.upper() in commands.keys():
-            commands[word.upper()]()
-            break
+            if commands[word.upper()] == my_name:
+                my_name(name)
+            else:
+                commands[word.upper()]()
+            break  
     else:
         sorry()
